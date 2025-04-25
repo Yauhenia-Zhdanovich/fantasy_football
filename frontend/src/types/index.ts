7d2dbbb74e996c59f53046a0e2eca458c3134a26
Ivan Belikov <belikov.workmail@gmail.com>
2025-04-25 10:48:12 +0300

    chore(types): ImageProps interface implemented

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,1 +1,2 @@
 export type { Nullable } from './types'
+export type { ImageProps } from './types'
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,1 +1,8 @@
+import type { BoxProps } from '@mui/material'
+
 export type Nullable<T> = T | null
+
+export interface ImageProps extends BoxProps {
+  src: string
+  alt: string
+}
